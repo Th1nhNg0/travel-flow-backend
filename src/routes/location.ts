f29442fc04cb4ef480a0429cc38623314c8aaafc
th1nhng0 <ngophuthinh2001@gmail.com>
2022-05-09 10:03:12 +0700

    update

--- src/routes/location.ts
+++ src/routes/location.ts
@@ -40,7 +40,7 @@ router.get("/", async (req, res) => {
   const locations: any = await prisma.location.findMany({
     take: limit,
     skip: offset,
-    select: {
+    include: {
       _count: {
         select: {
           Review: true,
